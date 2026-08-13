48083e545f0a4e4b5d7c281619746310583478bc
Axel Ignacio <ignacioaxelrom@gmail.com>
2026-08-13 10:57:45 -0400

    feat: add padContentLength to stings util

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -5,3 +5,7 @@ export function slugify(title: string) {
     .replace(/[^a-z0-9\s-]/g, '')
     .replace(/\s+/g, '-');
 }
+
+export function padContentLength(length: number) {
+  return String(length ?? 0).padStart(2, '0');
+}
